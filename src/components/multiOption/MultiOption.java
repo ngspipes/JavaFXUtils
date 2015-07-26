326ba4bfac0e4cbcfefa7a4d3077a5f4f033e6bd
BrunoMNDantas <bmndantas@gmail.com>
2015-07-26 18:53:31 +0100

    MultiOption : component to help create menus on Control objects

---
+++ src/components/multiOption/MultiOption.java
@@ -0,0 +1,46 @@
+package components.multiOption;
+
+import java.util.Collection;
+
+import javafx.scene.control.ContextMenu;
+import javafx.scene.control.Control;
+import javafx.scene.control.MenuItem;
+import javafx.util.Pair;
+
+import components.Component;
+import components.multiOption.Operations.Operation;
+
+public class MultiOption extends Component{
+	
+	private final Operations operations;
+	private final Control control;
+	
+	// Constructors
+	
+	public MultiOption(Control control, Operations operations) {
+		super(control);
+		this.operations = operations;
+		this.control = control;
+	}
+	
+	public MultiOption(Control control, Collection<Pair<String, Runnable>> actions) {
+		this(control, new Operations(actions));
+	}
+	
+	
+	// Implementation
+	
+	public void mount(){
+		ContextMenu menu = new ContextMenu();
+		
+		MenuItem menuItem;
+		for(Operation operation : operations.getOperations()){
+			menuItem = new MenuItem(operation.getName());
+			menuItem.setOnAction((event) -> operation.getAction().run());
+			menu.getItems().add(menuItem);	
+		}
+		
+		control.setContextMenu(menu);
+	}
+
+}
---
+++ src/components/multiOption/Operations.java
@@ -0,0 +1,89 @@
+package components.multiOption;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+
+import javafx.util.Pair;
+
+
+public class Operations {
+
+	public static class Operation{
+		
+		private final String name;
+		private final Runnable action;
+		
+		public Operation(String name, Runnable action) {
+			this.name = name;
+			this.action = action;
+		}
+		
+		public String getName(){
+			return name;
+		}
+		
+		public Runnable getAction(){
+			return action;
+		}
+		
+	}
+	
+	
+	private final Map<String, Operation> ops = new HashMap<>();
+	private final Collection<Operation> operations = new LinkedList<>();
+	private final Collection<String> names = new LinkedList<>();
+	private int length = 0;
+	
+	//Constructors 
+	
+	public Operations(){}
+	
+	public Operations(Collection<Pair<String, Runnable>> actions){
+		for(Pair<String , Runnable> action : actions)
+			add(action.getKey(), action.getValue());
+	}
+	
+	
+	// Implementation
+	
+	public Collection<Operation> getOperations(){
+		return operations;
+	}
+	
+	public Collection<String> getNames(){
+		return names;
+	}
+	
+	public Operation getOperation(String name){
+		return ops.get(name);
+	}
+	
+	public int getLength(){
+		return length;
+	}
+	
+	public void add(Operation operation){
+		ops.put(operation.getName(), operation);
+		operations.add(operation);
+		names.add(operation.getName());
+		++length;
+	}
+	
+	public void add(String name, Runnable action){
+		add(new Operation(name, action));
+	}
+	
+	public void remove(Operation operation){
+		ops.remove(operation.getName());
+		operations.remove(operation);
+		names.remove(operation.getName());
+		--length;
+	}
+	
+	public void add(String name){
+		remove(getOperation(name));
+	}
+	
+}
