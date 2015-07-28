812be2621d473917bca0ee0309b9116866251da0
BrunoMNDantas <bmndantas@gmail.com>
2015-07-28 20:08:12 +0100

    PressAndPassAnimation : component to help deal with MouseEnter, MouseExit, MousePress and MouseRelease events

---
+++ src/components/animation/PressAndPassAnimation.java
@@ -0,0 +1,35 @@
+package components.animation;
+
+import javafx.event.EventHandler;
+import javafx.scene.Node;
+import javafx.scene.input.MouseEvent;
+
+import components.Component;
+
+public class PressAndPassAnimation extends Component{
+	
+	private final PressAnimation pressAnimation;
+	private final PassAnimation passAnimation;
+	
+	public PressAndPassAnimation(	Node node, 
+									EventHandler<? super MouseEvent> onPress, EventHandler<? super MouseEvent> onRelease,
+									EventHandler<? super MouseEvent> onEnter, EventHandler<? super MouseEvent> onExit,
+									boolean keepOldHandlers){
+		super(node);
+		this.pressAnimation = new PressAnimation(node, onPress, onRelease, keepOldHandlers);
+		this.passAnimation = new PassAnimation(node, onEnter, onExit, keepOldHandlers);	
+	}
+	
+	public PressAndPassAnimation(	Node node, 
+									EventHandler<? super MouseEvent> onPress, EventHandler<? super MouseEvent> onRelease,
+									EventHandler<? super MouseEvent> onEnter, EventHandler<? super MouseEvent> onExit){
+		this(node, onPress, onRelease, onEnter, onExit, false);
+	}
+
+	@Override
+	public void mount() {
+		pressAnimation.mount();
+		passAnimation.mount();
+	}
+
+}
