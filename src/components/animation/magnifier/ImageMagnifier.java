11e49ec8eb7d51d3d42cffd02221b9899a62eec8
BrunoMNDantas <bmndantas@gmail.com>
2015-07-28 21:51:38 +0100

    ImageMagnifier: component to magnify image on MouseEnter and reduce on MouseExit events

---
+++ src/components/animation/magnifier/ImageMagnifier.java
@@ -0,0 +1,35 @@
+package components.animation.magnifier;
+
+import javafx.scene.image.ImageView;
+
+import components.animation.PassAnimation;
+
+
+public class ImageMagnifier extends PassAnimation{
+	
+	private static final double DEFAULT_MAGNIFY_AMP = 1.5;
+	
+	// Constructors
+	
+	public ImageMagnifier(ImageView image, double magnifyAmp, boolean keepOldHandlers){
+		super(image,
+				(event)->{	image.fitWidthProperty().set(image.fitWidthProperty().get()*magnifyAmp);
+							image.fitHeightProperty().set(image.fitHeightProperty().get()*magnifyAmp);	},
+				(event)->{	image.fitWidthProperty().set(image.fitWidthProperty().get()/magnifyAmp);
+							image.fitHeightProperty().set(image.fitHeightProperty().get()/magnifyAmp);	},
+				keepOldHandlers);
+	}
+	
+	public ImageMagnifier(ImageView image, double magnifyAmp){
+		this(image, magnifyAmp, false);
+	}
+	
+	public ImageMagnifier(ImageView image, boolean keepOldHandlers){
+		this(image, DEFAULT_MAGNIFY_AMP, keepOldHandlers);
+	}
+	
+	public ImageMagnifier(ImageView image){
+		this(image, false);
+	}
+
+}
