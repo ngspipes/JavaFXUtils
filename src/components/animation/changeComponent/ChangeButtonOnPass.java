7c269032a9bcc7a774faa5bcdbd42a3c8cbf2e00
BrunoMNDantas <bmndantas@gmail.com>
2015-07-28 21:45:33 +0100

    ChangeButtonOnPass :  component to change button image on MouseEnter and MouseExit events

---
+++ src/components/animation/changeComponent/ChangeButtonOnPass.java
@@ -0,0 +1,41 @@
+package components.animation.changeComponent;
+
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+import components.animation.PassAnimation;
+
+
+public class ChangeButtonOnPass extends PassAnimation{
+	
+	// Constructors
+	
+	public ChangeButtonOnPass(Button button, ImageView onEnter, ImageView onExit, boolean keepOldHandlers) {
+		super(	button, 
+				(onEnter==null) ? null : (event)->button.setGraphic(onEnter),
+				(onExit==null) ?  null : (event)->button.setGraphic(onExit),
+				keepOldHandlers	);
+	}
+	
+	public ChangeButtonOnPass(Button button, ImageView onEnter, ImageView onExit) {
+		this(button, onEnter, onExit, false);
+	}
+	
+	public ChangeButtonOnPass(Button button, Image onEnter, Image onExit, boolean keepOldHandlers) {
+		this(button, onEnter==null? null:new ImageView(onEnter) , onExit==null?null:new ImageView(onExit), keepOldHandlers);
+	}
+	
+	public ChangeButtonOnPass(Button button, Image onEnter, Image onExit) {
+		this(button, onEnter, onExit, false);
+	}
+	
+	public ChangeButtonOnPass(Button button, String onEnterPath, String onExitPath, boolean keepOldHandlers) {
+		this(button, onEnterPath==null ? null : new Image(onEnterPath), onExitPath==null ? null : new Image(onExitPath), keepOldHandlers);
+	}
+	
+	public ChangeButtonOnPass(Button button, String onEnterPath, String onExitPath) {
+		this(button, onEnterPath , onExitPath, false);
+	}
+
+}
