c13b889a4bcd8f1ee2f8eb9f870ec1e209c0696f
BrunoMNDantas <bmndantas@gmail.com>
2015-07-28 20:07:19 +0100

    PressAnimation : component to help deal with node changes on MousePress and MouseRelease events

---
+++ src/components/animation/PressAnimation.java
@@ -0,0 +1,21 @@
+package components.animation;
+
+import javafx.event.EventHandler;
+import javafx.scene.Node;
+import javafx.scene.input.MouseEvent;
+
+public class PressAnimation extends Animation {
+	
+	public PressAnimation(Node node, EventHandler<? super MouseEvent> onPress, EventHandler<? super MouseEvent> onRelease, boolean keepOldHandlers){
+		super(	node, 
+				node.getOnMousePressed(), onPress,
+				node.getOnMouseReleased(), onRelease,
+				node::setOnMousePressed, node::setOnMouseReleased,
+				keepOldHandlers	);
+	}
+	
+	public PressAnimation(Node node, EventHandler<? super MouseEvent> onPress, EventHandler<? super MouseEvent> onRelease){
+		this(node, onPress, onRelease, false);
+	}
+
+}
