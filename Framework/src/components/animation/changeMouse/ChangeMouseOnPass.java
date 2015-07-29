f0e91fde96d9d40e6737d3737a9d1e12ecf63f8f
BrunoMNDantas <bmndantas@gmail.com>
2015-07-29 12:05:24 +0100

    ChangeMouseOnPass generic in T extends Node

--- Framework/src/components/animation/changeMouse/ChangeMouseOnPass.java
+++ Framework/src/components/animation/changeMouse/ChangeMouseOnPass.java
@@ -8,58 +8,58 @@ import javafx.scene.image.Image;
 import components.IComponent;
 import components.animation.PassAnimation;
 
-public class ChangeMouseOnPass extends PassAnimation{
+public class ChangeMouseOnPass<T extends Node> extends PassAnimation<T>{
 	
 	// Constructors
 	
-	public ChangeMouseOnPass(Node node, Cursor onEnter, Cursor onExit, boolean keepOldHandlers) {
+	public ChangeMouseOnPass(T node, Cursor onEnter, Cursor onExit, boolean keepOldHandlers) {
 		super(	node,
 				(onEnter==null) ? null : (event)->((Node) event.getSource()).setCursor(onEnter),
 				(onExit==null) ? null : (event)->((Node) event.getSource()).setCursor(onExit),
 				keepOldHandlers	);
 	}
 	
-	public ChangeMouseOnPass(Node node, Cursor onEnter, Cursor onExit) {
+	public ChangeMouseOnPass(T node, Cursor onEnter, Cursor onExit) {
 		this(node, onEnter, onExit, true);
 	}
 	
-	public ChangeMouseOnPass(Node node, Image onEnter, Image onExit, boolean keepOldHandlers) {
+	public ChangeMouseOnPass(T node, Image onEnter, Image onExit, boolean keepOldHandlers) {
 		this(node, onEnter==null ? null : new ImageCursor(onEnter), onExit==null ? null : new ImageCursor(onExit), keepOldHandlers);
 	}
 	
-	public ChangeMouseOnPass(Node node, Image onEnter, Image onExit) {
+	public ChangeMouseOnPass(T node, Image onEnter, Image onExit) {
 		this(node, onEnter, onExit, true);
 	}
 	
-	public ChangeMouseOnPass(Node node, String onEnterPath, String onExitPath, boolean keepOldHandlers) {
+	public ChangeMouseOnPass(T node, String onEnterPath, String onExitPath, boolean keepOldHandlers) {
 		this(node, onEnterPath==null ? null : new Image(onEnterPath), onExitPath==null ? null : new Image(onExitPath), keepOldHandlers);
 	}
 	
-	public ChangeMouseOnPass(Node node, String onEnterPath, String onExitPath) {
+	public ChangeMouseOnPass(T node, String onEnterPath, String onExitPath) {
 		this(node, onEnterPath, onExitPath, true);
 	}
 	
-	public ChangeMouseOnPass(IComponent component, Cursor onEnter, Cursor onExit, boolean keepOldHandlers) {
+	public ChangeMouseOnPass(IComponent<T> component, Cursor onEnter, Cursor onExit, boolean keepOldHandlers) {
 		this(component.getNode(), onEnter, onExit, keepOldHandlers);	
 	}
 	
-	public ChangeMouseOnPass(IComponent component, Cursor onEnter, Cursor onExit) {
+	public ChangeMouseOnPass(IComponent<T> component, Cursor onEnter, Cursor onExit) {
 		this(component.getNode(), onEnter, onExit);
 	}
 	
-	public ChangeMouseOnPass(IComponent component, Image onEnter, Image onExit, boolean keepOldHandlers) {
+	public ChangeMouseOnPass(IComponent<T> component, Image onEnter, Image onExit, boolean keepOldHandlers) {
 		this(component.getNode(), onEnter, onExit, keepOldHandlers);
 	}
 	
-	public ChangeMouseOnPass(IComponent component, Image onEnter, Image onExit) {
+	public ChangeMouseOnPass(IComponent<T> component, Image onEnter, Image onExit) {
 		this(component.getNode(), onEnter, onExit);
 	}
 	
-	public ChangeMouseOnPass(IComponent component, String onEnterPath, String onExitPath, boolean keepOldHandlers) {
+	public ChangeMouseOnPass(IComponent<T> component, String onEnterPath, String onExitPath, boolean keepOldHandlers) {
 		this(component.getNode(), onEnterPath, onExitPath, keepOldHandlers);
 	}
 	
-	public ChangeMouseOnPass(IComponent component, String onEnterPath, String onExitPath) {
+	public ChangeMouseOnPass(IComponent<T> component, String onEnterPath, String onExitPath) {
 		this(component.getNode(), onEnterPath, onExitPath);
 	}
 	
