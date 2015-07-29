3f1dde488b6bd41c6ea19f322aa5b158a9fc5b05
BrunoMNDantas <bmndantas@gmail.com>
2015-07-29 12:04:37 +0100

    ChangeButtonOnPass generic in T extends Button

--- Framework/src/components/animation/changeComponent/ChangeButtonOnPass.java
+++ Framework/src/components/animation/changeComponent/ChangeButtonOnPass.java
@@ -7,34 +7,34 @@ import javafx.scene.image.ImageView;
 import components.animation.PassAnimation;
 
 
-public class ChangeButtonOnPass extends PassAnimation{
+public class ChangeButtonOnPass<T extends Button> extends PassAnimation<T>{
 	
 	// Constructors
 	
-	public ChangeButtonOnPass(Button button, ImageView onEnter, ImageView onExit, boolean keepOldHandlers) {
+	public ChangeButtonOnPass(T button, ImageView onEnter, ImageView onExit, boolean keepOldHandlers) {
 		super(	button, 
 				(onEnter==null) ? null : (event)->button.setGraphic(onEnter),
 				(onExit==null) ?  null : (event)->button.setGraphic(onExit),
 				keepOldHandlers	);
 	}
 	
-	public ChangeButtonOnPass(Button button, ImageView onEnter, ImageView onExit) {
+	public ChangeButtonOnPass(T button, ImageView onEnter, ImageView onExit) {
 		this(button, onEnter, onExit, true);
 	}
 	
-	public ChangeButtonOnPass(Button button, Image onEnter, Image onExit, boolean keepOldHandlers) {
+	public ChangeButtonOnPass(T button, Image onEnter, Image onExit, boolean keepOldHandlers) {
 		this(button, onEnter==null? null:new ImageView(onEnter) , onExit==null?null:new ImageView(onExit), keepOldHandlers);
 	}
 	
-	public ChangeButtonOnPass(Button button, Image onEnter, Image onExit) {
+	public ChangeButtonOnPass(T button, Image onEnter, Image onExit) {
 		this(button, onEnter, onExit, true);
 	}
 	
-	public ChangeButtonOnPass(Button button, String onEnterPath, String onExitPath, boolean keepOldHandlers) {
+	public ChangeButtonOnPass(T button, String onEnterPath, String onExitPath, boolean keepOldHandlers) {
 		this(button, onEnterPath==null ? null : new Image(onEnterPath), onExitPath==null ? null : new Image(onExitPath), keepOldHandlers);
 	}
 	
-	public ChangeButtonOnPass(Button button, String onEnterPath, String onExitPath) {
+	public ChangeButtonOnPass(T button, String onEnterPath, String onExitPath) {
 		this(button, onEnterPath , onExitPath, true);
 	}
 
