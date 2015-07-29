c72341dec2cd5fcf31bc119a8bb35bee905dd648
BrunoMNDantas <bmndantas@gmail.com>
2015-07-29 11:56:59 +0100

    PressAndPassAnimation generic in T extends Node

--- Framework/src/components/animation/PressAndPassAnimation.java
+++ Framework/src/components/animation/PressAndPassAnimation.java
@@ -7,38 +7,38 @@ import javafx.scene.input.MouseEvent;
 import components.Component;
 import components.IComponent;
 
-public class PressAndPassAnimation extends Component{
+public class PressAndPassAnimation<T extends Node> extends Component<T>{
 	
-	private final PressAnimation pressAnimation;
-	private final PassAnimation passAnimation;
+	private final PressAnimation<T> pressAnimation;
+	private final PassAnimation<T> passAnimation;
 	
 	// Constructors
 	
-	public PressAndPassAnimation(	Node node, 
+	public PressAndPassAnimation(	T node, 
 									EventHandler<? super MouseEvent> onPress, EventHandler<? super MouseEvent> onRelease,
 									EventHandler<? super MouseEvent> onEnter, EventHandler<? super MouseEvent> onExit,
-									boolean keepOldHandlers){
+									boolean keepOldHandlers	){
 		super(node);
-		this.pressAnimation = new PressAnimation(node, onPress, onRelease, keepOldHandlers);
-		this.passAnimation = new PassAnimation(node, onEnter, onExit, keepOldHandlers);	
+		this.pressAnimation = new PressAnimation<>(node, onPress, onRelease, keepOldHandlers);
+		this.passAnimation = new PassAnimation<>(node, onEnter, onExit, keepOldHandlers);	
 	}
 	
-	public PressAndPassAnimation(	Node node, 
+	public PressAndPassAnimation(	T node, 
 									EventHandler<? super MouseEvent> onPress, EventHandler<? super MouseEvent> onRelease,
-									EventHandler<? super MouseEvent> onEnter, EventHandler<? super MouseEvent> onExit){
+									EventHandler<? super MouseEvent> onEnter, EventHandler<? super MouseEvent> onExit	){
 		this(node, onPress, onRelease, onEnter, onExit, true);
 	}
 
-	public PressAndPassAnimation(	IComponent component, 
+	public PressAndPassAnimation(	IComponent<T> component, 
 									EventHandler<? super MouseEvent> onPress, EventHandler<? super MouseEvent> onRelease,
 									EventHandler<? super MouseEvent> onEnter, EventHandler<? super MouseEvent> onExit,
-			boolean keepOldHandlers){
+									boolean keepOldHandlers	){
 		this(component.getNode(), onPress, onRelease, onEnter, onExit, keepOldHandlers);	
 	}
 
-	public PressAndPassAnimation(	IComponent component, 
+	public PressAndPassAnimation(	IComponent<T> component, 
 									EventHandler<? super MouseEvent> onPress, EventHandler<? super MouseEvent> onRelease,
-									EventHandler<? super MouseEvent> onEnter, EventHandler<? super MouseEvent> onExit){
+									EventHandler<? super MouseEvent> onEnter, EventHandler<? super MouseEvent> onExit	){
 		this(component.getNode(), onPress, onRelease, onEnter, onExit);
 	}
 
