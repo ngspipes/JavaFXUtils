b0483b0c356797481cd05757b4c3eb359161c117
BrunoMNDantas <bmndantas@gmail.com>
2015-07-29 11:33:01 +0100

    DoubleClickable generic in T extends Node

--- Framework/src/components/DoubleClickable.java
+++ Framework/src/components/DoubleClickable.java
@@ -7,44 +7,44 @@ import javafx.scene.Node;
 import javafx.scene.input.MouseEvent;
 import utils.Utils;
 
-public class DoubleClickable extends Component{
+public class DoubleClickable<T extends Node> extends Component<T>{
 	
 	private final Consumer<MouseEvent> action;
 	private final boolean keepOldHandler;
 	
 	// Constructors
 	
-	public DoubleClickable(Node node, Consumer<MouseEvent> action, boolean keepOldHandler) {
+	public DoubleClickable(T node, Consumer<MouseEvent> action, boolean keepOldHandler) {
 		super(node);
 		this.action = action;
 		this.keepOldHandler = keepOldHandler;
 	}
 	
-	public DoubleClickable(Node node, Runnable action, boolean keepOldHandler) {
+	public DoubleClickable(T node, Runnable action, boolean keepOldHandler) {
 		this(node, (event)->action.run(), keepOldHandler);
 	}
 	
-	public DoubleClickable(IComponent component, Consumer<MouseEvent> action, boolean keepOldHandler) {
+	public DoubleClickable(IComponent<T> component, Consumer<MouseEvent> action, boolean keepOldHandler) {
 		this(component.getNode(), action, keepOldHandler);
 	}
 	
-	public DoubleClickable(IComponent component, Runnable action, boolean keepOldHandler) {
+	public DoubleClickable(IComponent<T> component, Runnable action, boolean keepOldHandler) {
 		this(component.getNode(), (event)->action.run(), keepOldHandler);
 	}
 	
-	public DoubleClickable(Node node, Consumer<MouseEvent> action) {
+	public DoubleClickable(T node, Consumer<MouseEvent> action) {
 		this(node, action, true);
 	}
 	
-	public DoubleClickable(IComponent component, Consumer<MouseEvent> action) {
+	public DoubleClickable(IComponent<T> component, Consumer<MouseEvent> action) {
 		this(component.getNode(), action);
 	}
 	
-	public DoubleClickable(Node node, Runnable action) {
+	public DoubleClickable(T node, Runnable action) {
 		this(node, (event)->action.run());
 	}
 	
-	public DoubleClickable(IComponent component, Runnable action) {
+	public DoubleClickable(IComponent<T> component, Runnable action) {
 		this(component.getNode(), action);
 	}
 
