e3334c80151dce91b68df09cb3bb8cd0ed60c53a
BrunoMNDantas <bmndantas@gmail.com>
2015-07-29 12:05:45 +0100

    ChangeMouseOnPress generic in T extends Node

--- Framework/src/components/animation/changeMouse/ChangeMouseOnPress.java
+++ Framework/src/components/animation/changeMouse/ChangeMouseOnPress.java
@@ -8,58 +8,58 @@ import javafx.scene.image.Image;
 import components.IComponent;
 import components.animation.PressAnimation;
 
-public class ChangeMouseOnPress extends PressAnimation{
+public class ChangeMouseOnPress<T extends Node> extends PressAnimation<T>{
 	
 	// Constructors
 	
-	public ChangeMouseOnPress(Node node, Cursor onPress, Cursor onRelease, boolean keepOldHandlers) {
+	public ChangeMouseOnPress(T node, Cursor onPress, Cursor onRelease, boolean keepOldHandlers) {
 		super(	node,
 				(onPress==null) ? null : (event)->((Node) event.getSource()).setCursor(onPress),
 				(onRelease==null) ? null : (event)->((Node) event.getSource()).setCursor(onRelease),
 				keepOldHandlers	);
 	}
 	
-	public ChangeMouseOnPress(Node node, Cursor onPress, Cursor onRelease) {
+	public ChangeMouseOnPress(T node, Cursor onPress, Cursor onRelease) {
 		this(node, onPress, onRelease, true);
 	}
 	
-	public ChangeMouseOnPress(Node node, Image onPress, Image onRelease, boolean keepOldHandlers) {
+	public ChangeMouseOnPress(T node, Image onPress, Image onRelease, boolean keepOldHandlers) {
 		this(node, onPress==null ? null : new ImageCursor(onPress), onRelease==null ? null : new ImageCursor(onRelease), keepOldHandlers);
 	}
 	
-	public ChangeMouseOnPress(Node node, Image onPress, Image onRelease) {
+	public ChangeMouseOnPress(T node, Image onPress, Image onRelease) {
 		this(node, onPress, onRelease, true);
 	}
 	
-	public ChangeMouseOnPress(Node node, String onPressPath, String onReleasePath, boolean keepOldHandlers) {
+	public ChangeMouseOnPress(T node, String onPressPath, String onReleasePath, boolean keepOldHandlers) {
 		this(node, onPressPath==null ? null : new Image(onPressPath), onReleasePath==null ? null : new Image(onReleasePath), keepOldHandlers);
 	}
 	
-	public ChangeMouseOnPress(Node node, String onPressPath, String onReleasePath) {
+	public ChangeMouseOnPress(T node, String onPressPath, String onReleasePath) {
 		this(node, onPressPath, onReleasePath, true);
 	}
 	
-	public ChangeMouseOnPress(IComponent component, Cursor onPress, Cursor onRelease, boolean keepOldHandlers) {
+	public ChangeMouseOnPress(IComponent<T> component, Cursor onPress, Cursor onRelease, boolean keepOldHandlers) {
 		this(component.getNode(), onPress, onRelease, keepOldHandlers);
 	}
 	
-	public ChangeMouseOnPress(IComponent component, Cursor onPress, Cursor onRelease) {
+	public ChangeMouseOnPress(IComponent<T> component, Cursor onPress, Cursor onRelease) {
 		this(component.getNode(), onPress, onRelease);
 	}
 	
-	public ChangeMouseOnPress(IComponent component, Image onPress, Image onRelease, boolean keepOldHandlers) {
+	public ChangeMouseOnPress(IComponent<T> component, Image onPress, Image onRelease, boolean keepOldHandlers) {
 		this(component.getNode(), onPress, onRelease, keepOldHandlers);
 	}
 	
-	public ChangeMouseOnPress(IComponent component, Image onPress, Image onRelease) {
+	public ChangeMouseOnPress(IComponent<T> component, Image onPress, Image onRelease) {
 		this(component.getNode(), onPress, onRelease);
 	}
 	
-	public ChangeMouseOnPress(IComponent component, String onPressPath, String onReleasePath, boolean keepOldHandlers) {
+	public ChangeMouseOnPress(IComponent<T> component, String onPressPath, String onReleasePath, boolean keepOldHandlers) {
 		this(component.getNode(), onPressPath, onReleasePath, keepOldHandlers);
 	}
 	
-	public ChangeMouseOnPress(IComponent component, String onPressPath, String onReleasePath) {
+	public ChangeMouseOnPress(IComponent<T> component, String onPressPath, String onReleasePath) {
 		this(component.getNode(), onPressPath, onReleasePath);
 	}
 
