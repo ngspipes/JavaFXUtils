9bb115b3ccfdc9b9b78454a36250972873c229d0
BrunoMNDantas <bmndantas@gmail.com>
2015-07-29 22:38:34 +0100

    ButtonMounter demo

---
+++ Demos/ButtonMounter/Project/src/demo/Demo.java
@@ -0,0 +1,86 @@
+package demo;
+
+import java.net.URL;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+import utils.ComponentException;
+
+import components.mounter.ButtonMounter;
+
+public class Demo extends Application {
+	
+	private static final String FXML_DOCUMENT_PATH = "resources/FXMLDocument.fxml";
+	private static final String TITLE = "ButtonMounter";
+	private static final String DESCRIPTION = "Demo to show usage of ButtonMounter component.\nIt was mounted over button below:\n - ButtonMagnifier\n - DoubleClickable\n - Movable.";
+	
+	
+	@Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader loader = getLoader();
+        
+        init(stage, loader);
+        
+        mount(loader.getController());
+    }
+	
+	public static FXMLLoader getLoader(){
+		URL location = ClassLoader.getSystemResource(FXML_DOCUMENT_PATH);
+		return new FXMLLoader(location);
+	}
+
+	private static void init(Stage stage, FXMLLoader loader) throws Exception{
+		Parent root = loader.load();
+		
+		Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();	
+	}
+	
+	private static void mount(Controller controller) throws Exception{
+		controller.title.setText(TITLE);
+		controller.description.setText(DESCRIPTION);
+		controller.description.editableProperty().set(false);
+		mountDemo(controller.demoPane);
+	}
+	
+	////////////////////////////////////////////////////////
+	//                                                    //
+	//                       DEMO                         //
+	//                                                    //
+	//    mountDemo(SubScene) -> relevant method          // 
+	//                           to see Component         //
+	//                           utilization              //
+	//                                                    //
+ 	//                                                    //
+	////////////////////////////////////////////////////////
+
+	private static void mountDemo(AnchorPane demoPane) {
+		Button button = new Button("Button");
+		
+		try {
+			new ButtonMounter<>(button)
+				.buttonMagnifier(1.15)
+				.movable()
+				.doubleClickable(()->button.setText(button.getText()+"A"))
+				.mount();
+		} catch (ComponentException e) {
+			Label error = new Label();
+			error.setText(e.getMessage());
+			demoPane.getChildren().add(error);
+		}	
+		
+		demoPane.getChildren().add(button);
+	}
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
