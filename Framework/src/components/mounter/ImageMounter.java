be0c177ad42e264de2424bf0b7d39eb555defa41
BrunoMNDantas <bmndantas@gmail.com>
2015-07-29 12:16:55 +0100

    ImageMounter generic in T extends ImageView

--- Framework/src/components/mounter/ImageMounter.java
+++ Framework/src/components/mounter/ImageMounter.java
@@ -4,32 +4,32 @@ import javafx.scene.image.ImageView;
 
 import components.animation.magnifier.ImageMagnifier;
 
-public class ImageMounter extends ComponentMounter{
+public class ImageMounter<T extends ImageView> extends ComponentMounter<T>{
 
-	private final ImageView image;
+	private final T image;
 	
-	public ImageMounter(ImageView image) {
+	public ImageMounter(T image) {
 		super(image);
 		this.image = image;
 	}
 
-	public ImageMounter imageMagnifier(double magnifyAmp, boolean keepOldHandlers){
-		this.components.add(new ImageMagnifier(image, magnifyAmp, keepOldHandlers));
+	public ImageMounter<T> imageMagnifier(double magnifyAmp, boolean keepOldHandlers){
+		this.components.add(new ImageMagnifier<>(image, magnifyAmp, keepOldHandlers));
 		return this;
 	}
 	
-	public ImageMounter imageMagnifier(double magnifyAmp){
-		this.components.add(new ImageMagnifier(image, magnifyAmp));
+	public ImageMounter<T> imageMagnifier(double magnifyAmp){
+		this.components.add(new ImageMagnifier<>(image, magnifyAmp));
 		return this;
 	}
 	
-	public ImageMounter imageMagnifier(boolean keepOldHandlers){
-		this.components.add(new ImageMagnifier(image, keepOldHandlers));
+	public ImageMounter<T> imageMagnifier(boolean keepOldHandlers){
+		this.components.add(new ImageMagnifier<>(image, keepOldHandlers));
 		return this;
 	}
 	
-	public ImageMounter imageMagnifier(){
-		this.components.add(new ImageMagnifier(image));
+	public ImageMounter<T> imageMagnifier(){
+		this.components.add(new ImageMagnifier<>(image));
 		return this;
 	}
 	
