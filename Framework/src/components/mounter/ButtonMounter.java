38b367af3632d8177f83c036e01f7af5fdffe2fd
BrunoMNDantas <bmndantas@gmail.com>
2015-07-29 12:16:30 +0100

    ButtonMounter generic in T extends Button

--- Framework/src/components/mounter/ButtonMounter.java
+++ Framework/src/components/mounter/ButtonMounter.java
@@ -8,93 +8,93 @@ import components.animation.changeComponent.ChangeButtonOnPass;
 import components.animation.changeComponent.ChangeButtonOnPress;
 import components.animation.magnifier.ButtonMagnifier;
 
-public class ButtonMounter extends ControlMounter{
+public class ButtonMounter<T extends Button> extends ControlMounter<T>{
 	
-	private final Button button;
+	private final T button;
 	
-	public ButtonMounter(Button button){
+	public ButtonMounter(T button){
 		super(button);
 		this.button = button;
 	}
 
 	
-	public ButtonMounter changeButtonOnPress(ImageView onPress, ImageView onRelease, boolean keepOldHandlers) {
-		this.components.add(new ChangeButtonOnPress(button, onPress, onRelease, keepOldHandlers));
+	public ButtonMounter<T> changeButtonOnPress(ImageView onPress, ImageView onRelease, boolean keepOldHandlers) {
+		this.components.add(new ChangeButtonOnPress<>(button, onPress, onRelease, keepOldHandlers));
 		return this;
 	}
 	
-	public ButtonMounter changeButtonOnPress(ImageView onPress, ImageView onRelease) {
-		this.components.add(new ChangeButtonOnPress(button, onPress, onRelease));
+	public ButtonMounter<T> changeButtonOnPress(ImageView onPress, ImageView onRelease) {
+		this.components.add(new ChangeButtonOnPress<>(button, onPress, onRelease));
 		return this;
 	}
 	
-	public ButtonMounter changeButtonOnPress(Image onPress, Image onRelease, boolean keepOldHandlers) {
-		this.components.add(new ChangeButtonOnPress(button, onPress, onRelease, keepOldHandlers));
+	public ButtonMounter<T> changeButtonOnPress(Image onPress, Image onRelease, boolean keepOldHandlers) {
+		this.components.add(new ChangeButtonOnPress<>(button, onPress, onRelease, keepOldHandlers));
 		return this;
 	}
 	
-	public ButtonMounter changeButtonOnPress(Image onPress, Image onRelease) {
-		this.components.add(new ChangeButtonOnPress(button, onPress, onRelease));
+	public ButtonMounter<T> changeButtonOnPress(Image onPress, Image onRelease) {
+		this.components.add(new ChangeButtonOnPress<>(button, onPress, onRelease));
 		return this;
 	}
 	
-	public ButtonMounter changeButtonOnPress(String onPressPath, String onReleasePath, boolean keepOldHandlers) {
-		this.components.add(new ChangeButtonOnPress(button, onPressPath, onReleasePath, keepOldHandlers));
+	public ButtonMounter<T> changeButtonOnPress(String onPressPath, String onReleasePath, boolean keepOldHandlers) {
+		this.components.add(new ChangeButtonOnPress<>(button, onPressPath, onReleasePath, keepOldHandlers));
 		return this;
 	}
 	
-	public ButtonMounter changeButtonOnPress(String onPressPath, String onReleasePath) {
-		this.components.add(new ChangeButtonOnPress(button, onPressPath, onReleasePath));
+	public ButtonMounter<T> changeButtonOnPress(String onPressPath, String onReleasePath) {
+		this.components.add(new ChangeButtonOnPress<>(button, onPressPath, onReleasePath));
 		return this;
 	}
 	
-	public ButtonMounter changeButtonOnPass(ImageView onEnter, ImageView onExit, boolean keepOldHandlers) {
-		this.components.add(new ChangeButtonOnPass(button, onEnter, onExit, keepOldHandlers));
+	public ButtonMounter<T> changeButtonOnPass(ImageView onEnter, ImageView onExit, boolean keepOldHandlers) {
+		this.components.add(new ChangeButtonOnPass<>(button, onEnter, onExit, keepOldHandlers));
 		return this;
 	}
 	
-	public ButtonMounter changeButtonOnPass(ImageView onEnter, ImageView onExit) {
-		this.components.add(new ChangeButtonOnPass(button, onEnter, onExit));
+	public ButtonMounter<T> changeButtonOnPass(ImageView onEnter, ImageView onExit) {
+		this.components.add(new ChangeButtonOnPass<>(button, onEnter, onExit));
 		return this;
 	}
 	
-	public ButtonMounter changeButtonOnPass(Image onEnter, Image onExit, boolean keepOldHandlers) {
-		this.components.add(new ChangeButtonOnPass(button, onEnter, onExit, keepOldHandlers));
+	public ButtonMounter<T> changeButtonOnPass(Image onEnter, Image onExit, boolean keepOldHandlers) {
+		this.components.add(new ChangeButtonOnPass<>(button, onEnter, onExit, keepOldHandlers));
 		return this;
 	}
 	
-	public ButtonMounter changeButtonOnPass(Image onEnter, Image onExit) {
-		this.components.add(new ChangeButtonOnPass(button, onEnter, onExit));
+	public ButtonMounter<T> changeButtonOnPass(Image onEnter, Image onExit) {
+		this.components.add(new ChangeButtonOnPass<>(button, onEnter, onExit));
 		return this;
 	}
 	
-	public ButtonMounter changeButtonOnPass(String onEnterPath, String onExitPath, boolean keepOldHandlers) {
-		this.components.add(new ChangeButtonOnPass(button, onEnterPath, onExitPath, keepOldHandlers));
+	public ButtonMounter<T> changeButtonOnPass(String onEnterPath, String onExitPath, boolean keepOldHandlers) {
+		this.components.add(new ChangeButtonOnPass<>(button, onEnterPath, onExitPath, keepOldHandlers));
 		return this;
 	}
 	
-	public ButtonMounter changeButtonOnPass(String onEnterPath, String onExitPath) {
-		this.components.add(new ChangeButtonOnPass(button, onEnterPath, onExitPath));
+	public ButtonMounter<T> changeButtonOnPass(String onEnterPath, String onExitPath) {
+		this.components.add(new ChangeButtonOnPass<>(button, onEnterPath, onExitPath));
 		return this;
 	}
 
-	public ButtonMounter buttonMagnifier(double magnifyAmp, boolean keepOldHandlers){
-		this.components.add(new ButtonMagnifier(button, magnifyAmp, keepOldHandlers));
+	public ButtonMounter<T> buttonMagnifier(double magnifyAmp, boolean keepOldHandlers){
+		this.components.add(new ButtonMagnifier<>(button, magnifyAmp, keepOldHandlers));
 		return this;
 	}
 	
-	public ButtonMounter buttonMagnifier(double magnifyAmp){
-		this.components.add(new ButtonMagnifier(button, magnifyAmp));
+	public ButtonMounter<T> buttonMagnifier(double magnifyAmp){
+		this.components.add(new ButtonMagnifier<>(button, magnifyAmp));
 		return this;
 	}
 	
-	public ButtonMounter buttonMagnifier(boolean keepOldHandlers){
-		this.components.add(new ButtonMagnifier(button, keepOldHandlers));
+	public ButtonMounter<T> buttonMagnifier(boolean keepOldHandlers){
+		this.components.add(new ButtonMagnifier<>(button, keepOldHandlers));
 		return this;
 	}
 	
-	public ButtonMounter buttonMagnifier(){
-		this.components.add(new ButtonMagnifier(button));
+	public ButtonMounter<T> buttonMagnifier(){
+		this.components.add(new ButtonMagnifier<>(button));
 		return this;
 	}
 	
