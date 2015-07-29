08a836d005e5ba215a81e85fe012b114aaae4f34
BrunoMNDantas <bmndantas@gmail.com>
2015-07-29 12:04:57 +0100

    ChangeButtonOnPress generic in T extends Button

--- Framework/src/components/animation/changeComponent/ChangeButtonOnPress.java
+++ Framework/src/components/animation/changeComponent/ChangeButtonOnPress.java
@@ -7,34 +7,34 @@ import javafx.scene.image.ImageView;
 import components.animation.PressAnimation;
 
 
-public class ChangeButtonOnPress extends PressAnimation{
+public class ChangeButtonOnPress<T extends Button> extends PressAnimation<T>{
 
 	// Constructors
 	
-	public ChangeButtonOnPress(Button button, ImageView onPress, ImageView onRelease, boolean keepOldHandlers) {
+	public ChangeButtonOnPress(T button, ImageView onPress, ImageView onRelease, boolean keepOldHandlers) {
 		super(	button,
 				(onPress==null) ? null : (event)->button.setGraphic(onPress),
 				(onRelease==null) ?  null : (event)->button.setGraphic(onRelease), 
 				keepOldHandlers	);
 	}
 	
-	public ChangeButtonOnPress(Button button, ImageView onPress, ImageView onRelease) {
+	public ChangeButtonOnPress(T button, ImageView onPress, ImageView onRelease) {
 		this(button, onPress, onRelease, true);
 	}
 	
-	public ChangeButtonOnPress(Button button, Image onPress, Image onRelease, boolean keepOldHandlers) {
+	public ChangeButtonOnPress(T button, Image onPress, Image onRelease, boolean keepOldHandlers) {
 		this(button, onPress==null ? null : new ImageView(onPress), onRelease==null ? null : new ImageView(onRelease), keepOldHandlers);	
 	}
 	
-	public ChangeButtonOnPress(Button button, Image onPress, Image onRelease) {
+	public ChangeButtonOnPress(T button, Image onPress, Image onRelease) {
 		this(button, onPress, onRelease, true);
 	}
 	
-	public ChangeButtonOnPress(Button button, String onPressPath, String onReleasePath, boolean keepOldHandlers) {
+	public ChangeButtonOnPress(T button, String onPressPath, String onReleasePath, boolean keepOldHandlers) {
 		this(button, onPressPath==null ? null : new Image(onPressPath), onReleasePath==null ? null : new Image(onReleasePath), keepOldHandlers);
 	}
 	
-	public ChangeButtonOnPress(Button button, String onPressPath, String onReleasePath) {
+	public ChangeButtonOnPress(T button, String onPressPath, String onReleasePath) {
 		this(button, onPressPath, onReleasePath, true);
 	}
 	
