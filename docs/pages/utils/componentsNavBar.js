66e2ad51f9b7041400a72999dc4aa7b61cc26187
BrunoMNDantas <bmndantas@gmail.com>
2015-08-03 02:33:14 +0100

    docs

---
+++ docs/pages/utils/componentsNavBar.js
@@ -0,0 +1,37 @@
+document.write('\
+<div class="nav-docs">\
+\
+	<div class="nav-docs-section">\
+		<h3>Components</h3>\
+		<ul>\
+			<li><a href="">Animation</a></li>\
+			<li><a href="">ButtonMagnifier</a></li>\
+			<li><a href="">ChangeButtonOnPass</a></li>\
+			<li><a href="">ChangeButtonOnPress</a></li>\
+			<li><a href="">ChangeMouseOnPass</a></li>\
+			<li><a href="">ChangeMouseOnPress</a></li>\
+			<li><a href="">DoubleClickable</a></li>\
+			<li><a href="">FXMLFile</a></li>\
+			<li><a href="">ImageMagnifier</a></li>\
+			<li><a href="">Movable</a></li>\
+			<li><a href="">MultiOption</a></li>\
+			<li><a href="">PassAnimation</a></li>\
+			<li><a href="">PressAnimation</a></li>\
+			<li><a href="">PressAndPassAnimation</a></li>\
+			<li><a href="">Tip</a></li>\
+			<li><a href="">Window</a></li>\
+		</ul>\
+	</div>\
+\
+	<div class="nav-docs-section">\
+		<h3>Mounters</h3>\
+		<ul>\
+			<li><a href="">ButtonMounter</a></li>\
+			<li><a href="">ComponentMounter</a></li>\
+			<li><a href="">ControlMounter</a></li>\
+			<li><a href="">ImageMounter</a></li>\
+		</ul>\
+	</div>\
+\
+</div>\
+');
---
+++ docs/pages/utils/header.js
@@ -0,0 +1,17 @@
+document.write('\
+<div class="nav-main">\
+	<div class="wrap">\
+\
+		<a class="nav-home" href="http://brunomndantas.github.io/JavaFXUtils/index.html">JavaFXUtils</a>\
+\
+		<ul class="nav-site nav-site-internal">\
+			<li><a href="" class="active">Docs</a></li>\
+			<li><a href="">Download</a></li>\
+		</ul>\
+\
+		<ul class="nav-site nav-site-external">\
+			<li><a href="https://github.com/BrunoMNDantas/JavaFXUtils">GitHub</a></li>\
+		</li></ul>\
+	</div>\
+</div>\
+');
