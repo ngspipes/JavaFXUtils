8d4cee56a953b634f812dcf2d90cb8684023b3a5
BrunoMNDantas <bmndantas@gmail.com>
2015-08-03 03:37:09 +0100

    docs clean up

--- docs/pages/utils/componentsNavBar.js
+++ docs/pages/utils/componentsNavBar.js
@@ -4,32 +4,32 @@ document.write('\
 	<div class="nav-docs-section">\
 		<h3>Components</h3>\
 		<ul>\
-			<li><a href="./docs/pages/components/animation.html">Animation</a></li>\
-			<li><a href="./docs/pages/components/buttonMagnifier.html">ButtonMagnifier</a></li>\
-			<li><a href="./docs/pages/components/changeButtonOnPass.html">ChangeButtonOnPass</a></li>\
-			<li><a href="./docs/pages/components/changeButtonOnPress.html">ChangeButtonOnPress</a></li>\
-			<li><a href="./docs/pages/components/changeMouseOnPass.html">ChangeMouseOnPass</a></li>\
-			<li><a href="./docs/pages/components/changeMouseOnPress.html">ChangeMouseOnPress</a></li>\
-			<li><a href="./docs/pages/components/doubleClickble.html">DoubleClickable</a></li>\
-			<li><a href="./docs/pages/components/fXMLFile.html">FXMLFile</a></li>\
-			<li><a href="./docs/pages/components/imageMagnifier.html">ImageMagnifier</a></li>\
-			<li><a href="./docs/pages/components/movable.html">Movable</a></li>\
-			<li><a href="./docs/pages/components/multiOption.html">MultiOption</a></li>\
-			<li><a href="./docs/pages/components/passAnimation.html">PassAnimation</a></li>\
-			<li><a href="./docs/pages/components/pressAnimation.html">PressAnimation</a></li>\
-			<li><a href="./docs/pages/components/pressAndPassAnimation.html">PressAndPassAnimation</a></li>\
-			<li><a href="./docs/pages/components/tip.html">Tip</a></li>\
-			<li><a href="./docs/pages/components/window.html">Window</a></li>\
+			<li><a href="docs/pages/components/animation.html">Animation</a></li>\
+			<li><a href="docs/pages/components/buttonMagnifier.html">ButtonMagnifier</a></li>\
+			<li><a href="docs/pages/components/changeButtonOnPass.html">ChangeButtonOnPass</a></li>\
+			<li><a href="docs/pages/components/changeButtonOnPress.html">ChangeButtonOnPress</a></li>\
+			<li><a href="docs/pages/components/changeMouseOnPass.html">ChangeMouseOnPass</a></li>\
+			<li><a href="docs/pages/components/changeMouseOnPress.html">ChangeMouseOnPress</a></li>\
+			<li><a href="docs/pages/components/doubleClickble.html">DoubleClickable</a></li>\
+			<li><a href="docs/pages/components/fXMLFile.html">FXMLFile</a></li>\
+			<li><a href="docs/pages/components/imageMagnifier.html">ImageMagnifier</a></li>\
+			<li><a href="docs/pages/components/movable.html">Movable</a></li>\
+			<li><a href="docs/pages/components/multiOption.html">MultiOption</a></li>\
+			<li><a href="docs/pages/components/passAnimation.html">PassAnimation</a></li>\
+			<li><a href="docs/pages/components/pressAnimation.html">PressAnimation</a></li>\
+			<li><a href="docs/pages/components/pressAndPassAnimation.html">PressAndPassAnimation</a></li>\
+			<li><a href="docs/pages/components/tip.html">Tip</a></li>\
+			<li><a href="docs/pages/components/window.html">Window</a></li>\
 		</ul>\
 	</div>\
 \
 	<div class="nav-docs-section">\
 		<h3>Mounters</h3>\
 		<ul>\
-			<li><a href="./docs/pages/components/mounters/buttonMounter.html">ButtonMounter</a></li>\
-			<li><a href="./docs/pages/components/mounters/componentMounter.html">ComponentMounter</a></li>\
-			<li><a href="./docs/pages/components/mounters/controlMounter.html">ControlMounter</a></li>\
-			<li><a href="./docs/pages/components/mounters/imageMounter.html">ImageMounter</a></li>\
+			<li><a href="docs/pages/components/mounters/buttonMounter.html">ButtonMounter</a></li>\
+			<li><a href="docs/pages/components/mounters/componentMounter.html">ComponentMounter</a></li>\
+			<li><a href="docs/pages/components/mounters/controlMounter.html">ControlMounter</a></li>\
+			<li><a href="docs/pages/components/mounters/imageMounter.html">ImageMounter</a></li>\
 		</ul>\
 	</div>\
 \
