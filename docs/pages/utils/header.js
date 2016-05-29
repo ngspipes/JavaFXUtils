0205c015add7d87612c31a1abcaa0d8363a9a537
BrunoMNDantas <bmndantas@gmail.com>
2016-05-29 02:26:09 +0100

    Docs uris fixed

--- docs/pages/utils/header.js
+++ docs/pages/utils/header.js
@@ -2,7 +2,7 @@ document.write('\
 <div class="nav-main">\
 	<div class="wrap">\
 \
-		<a class="nav-home" href="http://brunomndantas.github.io/JavaFXUtils/index.html">JavaFXUtils</a>\
+		<a class="nav-home" href="http://ngspipes.github.io/JavaFXUtils/index.html">JavaFXUtils</a>\
 \
 		<ul class="nav-site nav-site-internal">\
 			<li><a id="header-docs" href="/JavaFXUtils/docs/pages/docs.html">Docs</a></li>\
@@ -10,7 +10,7 @@ document.write('\
 		</ul>\
 \
 		<ul class="nav-site nav-site-external">\
-			<li><a href="https://github.com/BrunoMNDantas/JavaFXUtils">GitHub</a></li>\
+			<li><a href="https://github.com/ngspipes/JavaFXUtils">GitHub</a></li>\
 		</li></ul>\
 	</div>\
 </div>\
